39d9d763ae1891d4de1c66f3f13f72f07d860cfb
NahuelLucana <nlucanadelgadillo@frba.utn.edu.ar>
2025-07-02 15:47:31 -0300

    Agrego funcionalidad de marcado en el item de header cuando se encuentra en la pagina, falta el icono de BIRBNB

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,7 @@ import {
   Divider,
   Drawer,
   Group,
-  HoverCard,
   ScrollArea,
-  SimpleGrid,
   Text,
   ThemeIcon,
   UnstyledButton,
@@ -20,102 +18,50 @@ import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './HeaderMegaMenu.module.css';
 import  Link from 'next/link';
-const mockdata = [
-];
+import { usePathname } from 'next/navigation';
+
 
 export default function Header() {
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-  const theme = useMantineTheme();
-
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={22} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
-
-  return (
-    <Box pb={120}>
-      <header className={classes.header}>
+    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+    const pathname = usePathname();
+    return (
+    <Box >
+        <header className={classes.header}>
         <Group justify="space-between" h="100%">
-          <MantineLogo size={30} />
-
-          <Group h="100%" gap={0} visibleFrom="sm">
-            <Link href="/" className={classes.link}>
-                Home
+            <Link href="/" className="navbar-brand">
+                Birbnb
             </Link>
-            <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-              <HoverCard.Target>
-                
-                <a href="#" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Features
-                    </Box>
-                  </Center>
-                </a>
-              </HoverCard.Target>
 
-              <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">
-                    View all
-                  </Anchor>
-                </Group>
+            <Group h="100%" gap={0} visibleFrom="sm">
+                <Link
+                    href="/"
+                    className={`${classes.link} ${pathname === '/' ? classes.active : ''}`}
+                    >
+                    Home
+                </Link>
 
-                <Divider my="sm" />
+                <Link
+                    href="/alojamientos"
+                    className={`${classes.link} ${pathname === '/alojamientos' ? classes.active : ''}`}
+                            >
+                    Alojamientos
+                </Link>
+            </Group>
 
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
+            <Group visibleFrom="sm">
+                <Link href="/auth/login" className="navbar-button">
+                    <Button variant="default">Login</Button>
+                </Link>
+                <Link href="/auth/register" className="navbar-button navbar-button-secondary">
+                    <Button>Registrar</Button>
+                </Link>
+            </Group>
 
-                <div className={classes.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" c="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard>
-            <Link href="/alojamientos" className={classes.link}>
-              Alojamientos
-            </Link>
-          </Group>
-
-          <Group visibleFrom="sm">
-            <Link href="/auth/login" className="navbar-button">
-                <Button variant="default">Log in</Button>
-            </Link>
-            <Link href="/auth/register" className="navbar-button navbar-button-secondary">
-                <Button>Sign up</Button>
-            </Link>
-          </Group>
-
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
         </Group>
-      </header>
+        </header>
 
-      <Drawer
+        <Drawer
         opened={drawerOpened}
         onClose={closeDrawer}
         size="100%"
@@ -123,36 +69,28 @@ export default function Header() {
         title="Navigation"
         hiddenFrom="sm"
         zIndex={1000000}
-      >
+        >
         <ScrollArea h="calc(100vh - 80px" mx="-md">
-          <Divider my="sm" />
+            <Divider my="sm" />
+            <Link href="/"  className={classes.link}>
+                Home
+            </Link>
+            <Link href="/alojamientos" className={classes.link}>
+                Alojamientos
+            </Link>
+            <Divider my="sm" />
 
-          <a href="#" className={classes.link}>
-            Home
-          </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                Features
-              </Box>
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
-          </a>
-          <a href="#" className={classes.link}>
-            Academy
-          </a>
-
-          <Divider my="sm" />
-
-          <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
-          </Group>
+            <Divider my="sm" />
+            <Group justify="center" grow pb="xl" px="md">
+            <Link href="/auth/login" className="navbar-button">
+                <Button variant="default">Log in</Button>
+            </Link>
+            <Link href="/auth/register" className="navbar-button navbar-button-secondary">
+                <Button>Sign up</Button>
+            </Link>
+            </Group>
         </ScrollArea>
-      </Drawer>
+        </Drawer>
     </Box>
   );
 }
